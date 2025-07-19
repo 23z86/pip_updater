59f1aa95a277581bbd93fdfe0c00a4e069c2ac50
Juergen Hardt <hardt.juergen.mail@web.de>
2025-07-19 17:19:46 +0200

    Remove IDE configuration files and implement PipUp class for package updates

---
+++ pipup.py
@@ -0,0 +1,21 @@
+from library.classes.package_updater import PackageUpdater
+from library.classes.common_update_strategy import CommonUpdateStrategy
+from library.classes.pip_update_strategy import PipUpdateStrategy
+
+
+class PipUp:
+    def __init__(self, package_name):
+        self.package_name = package_name
+        self.updater = PackageUpdater()
+
+    def update_pip(self):
+        self.updater.set_strategy(PipUpdateStrategy())
+        self.updater.update(self.package_name)
+
+    def update_common(self):
+        self.updater.set_strategy(CommonUpdateStrategy())
+        self.updater.update(self.package_name)
+
+
+o_pipup = PipUp("aiohttp")
+o_pipup.update_common()
